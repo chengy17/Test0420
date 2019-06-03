d969e5b416238a27c57a7ba20326c371214c8469
chengy17 <chengy_95@163.com>
2019-06-03 09:33:58 +0800

    add rgb

--- main.ts
+++ main.ts
@@ -10,6 +10,16 @@ load dependency
 //% color="#228B22" weight=25 icon="\uf0b2"
 namespace CrocoKit_Sensor {
 
+    const COLOR_ADD = 0x53;
+    const COLOR_REG = 0x00;
+    const COLOR_R = 0x10;
+    const COLOR_G = 0x0D;
+    const COLOR_B = 0x13;
+
+    let initialized = false;
+    let val_red = 0;
+    let val_green = 0;
+    let val_blue = 0;
   
     export enum enObstacle {
         //% blockId="Obstacle" block="Obstacle"
@@ -17,6 +27,71 @@ namespace CrocoKit_Sensor {
         //% blockId="NoObstacle" block="NoObstacle"
         NoObstacle = 1
     }
+	
+	export enum enGetRGB {
+        //% blockId="GetValueR" block="GetValueR"
+        GetValueR = 0,
+        //% blockId="GetValueG" block="GetValueG"
+        GetValueG = 1,
+        //% blockId="GetValueB" block="GetValueB"
+        GetValueB = 2
+    }
+	
+	function setRegConfig(): void {
+        i2cwrite(COLOR_ADD, COLOR_REG, 0x06);
+        i2cwrite(COLOR_ADD, 0x04, 0x41);
+        i2cwrite(COLOR_ADD, 0x05, 0x01);
+    }
+	
+	function initColorI2C(): void {
+        setRegConfig();
+        initialized = true;
+    }
+	
+	function GetRGB(): void {
+        pins.i2cWriteNumber(COLOR_ADD, COLOR_R, NumberFormat.UInt8BE);
+        let buff_R = pins.i2cReadBuffer(COLOR_ADD, 2);
+
+        pins.i2cWriteNumber(COLOR_ADD, COLOR_G, NumberFormat.UInt8BE);
+        let buff_G = pins.i2cReadBuffer(COLOR_ADD, 2);
+
+        pins.i2cWriteNumber(COLOR_ADD, COLOR_B, NumberFormat.UInt8BE);
+        let buff_B = pins.i2cReadBuffer(COLOR_ADD, 2);
+
+        let Red = (buff_R[1] & 0xff) << 8 | (buff_R[0] & 0xff);
+        let Green = (buff_G[1] & 0xff) << 8 | (buff_G[0] & 0xff);
+        let Blue = (buff_B[1] & 0xff) << 8 | (buff_B[0] & 0xff);
+
+        if (Red > 4500) Red = 2300;
+        if (Green > 7600) Green = 4600;
+        if (Blue > 4600) Blue = 2700;
+
+        val_red = Math.map(Red, 0, 2300, 0, 255);
+        val_green = Math.map(Green, 0, 4600, 0, 255);
+        val_blue = Math.map(Blue, 0, 2700, 0, 255);
+
+        if (val_red == val_green && val_red == val_blue) {
+            val_red = 255;
+            val_green = 255;
+            val_blue == 255;
+        }
+        else if (val_red > val_green && val_red > val_blue) {
+            val_red = 255;
+            val_green /= 2;
+            val_blue /= 2;
+        }
+        else if (val_green > val_red && val_green > val_blue) {
+            val_green = 255;
+            val_red /= 2;
+            val_blue /= 2;
+        }
+        else if (val_blue > val_red && val_blue > val_green) {
+            val_blue = 255;
+            val_red /= 2;
+            val_green /= 2;
+        }
+    }
+	
 
     //% blockId=CrocoKit_Sensor_Light block="Light|pin %pin"
     //% weight=100
@@ -40,6 +115,31 @@ namespace CrocoKit_Sensor {
         return value;
     }
 
+	//% blockId=CrocoKit_Sensor_GetRGBValue block="GetRGBValue |value %value"
+    //% blockGap=20
+    //% weight=98
+    //% color="#228B22"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function GetRGBValue(value: enGetRGB): number {
+        if (!initialized) {
+            initColorI2C();
+        }
+        switch (value) {
+            case enGetRGB.GetValueR:
+                GetRGB();
+                return val_red;
+            case enGetRGB.GetValueG:
+                GetRGB();
+                return val_green;
+            case enGetRGB.GetValueB:
+                GetRGB();
+                return val_blue;
+            default:
+                break;
+        }
+        return 0;
+    }
+	
     //% blockId=CrocoKit_Sensor_IR block="IR|pin %pin|value %value"
     //% weight=97
     //% blockGap=20
