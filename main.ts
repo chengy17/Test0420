685fd56ddb5a62c12da3072d4e71a86249797bbf
chengy17 <chengy_95@163.com>
2019-06-21 10:15:02 +0800

    TEST RGB

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color="#228B22" weight=25 icon="\uf0b2"
+﻿//% color="#228B22" weight=25 icon="\uf0b2"
 namespace CrocoKit_Sensor {
 
     const COLOR_ADD = 0X53;
@@ -21,13 +21,7 @@ namespace CrocoKit_Sensor {
         GetValueB = 2
     }
 
-    export enum enObstacle {
-        //% blockId="Obstacle" block="Obstacle"
-        Obstacle = 0,
-        //% blockId="NoObstacle" block="NoObstacle"
-        NoObstacle = 1
-    }
-
+    
     function i2cWriteData(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2);
         buf[0] = reg;
@@ -64,59 +58,42 @@ namespace CrocoKit_Sensor {
         let Green = (buff_G[1] & 0xff) << 8 | (buff_G[0] & 0xff);
         let Blue = (buff_B[1] & 0xff) << 8 | (buff_B[0] & 0xff);
 
-        if (Red > 4500) Red = 2300;
-        if (Green > 7600) Green = 4600;
-        if (Blue > 4600) Blue = 2700;
+        val_red = Red;
+        val_green = Green;
+        val_blue = Blue;
 
-        val_red = Math.map(Red, 0, 2300, 0, 255);
-        val_green = Math.map(Green, 0, 4600, 0, 255);
-        val_blue = Math.map(Blue, 0, 2700, 0, 255);
 
-        if (val_red == val_green && val_red == val_blue) {
-            val_red = 255;
-            val_green = 255;
-            val_blue == 255;
-        }
-        else if (val_red > val_green && val_red > val_blue) {
-            val_red = 255;
-            val_green /= 2;
-            val_blue /= 2;
-        }
-        else if (val_green > val_red && val_green > val_blue) {
-            val_green = 255;
-            val_red /= 2;
-            val_blue /= 2;
-        }
-        else if (val_blue > val_red && val_blue > val_green) {
-            val_blue = 255;
-            val_red /= 2;
-            val_green /= 2;
-        }
+        // if (Red > 4500) Red = 2300;
+        // if (Green > 7600) Green = 4600;
+        // if (Blue > 4600) Blue = 2700;
+
+        // val_red = Math.map(Red, 0, 2300, 0, 255);
+        // val_green = Math.map(Green, 0, 4600, 0, 255);
+        // val_blue = Math.map(Blue, 0, 2700, 0, 255);
+
+        // if (val_red == val_green && val_red == val_blue) {
+        //     val_red = 255;
+        //     val_green = 255;
+        //     val_blue == 255;
+        // }
+        // else if (val_red > val_green && val_red > val_blue) {
+        //     val_red = 255;
+        //     val_green /= 2;
+        //     val_blue /= 2;
+        // }
+        // else if (val_green > val_red && val_green > val_blue) {
+        //     val_green = 255;
+        //     val_red /= 2;
+        //     val_blue /= 2;
+        // }
+        // else if (val_blue > val_red && val_blue > val_green) {
+        //     val_blue = 255;
+        //     val_red /= 2;
+        //     val_green /= 2;
+        // }
     }
 
-
-    //% blockId=CrocoKit_Sensor_Light block="Light|pin %pin"
-    //% weight=100
-    //% blockGap=20
-    //% color="#228B22"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5 
-    export function Light(pin: AnalogPin): number {
-        let value: number;
-        value = pins.analogReadPin(pin);
-        return value;
-    }
-
-    //% blockId=CrocoKit_Sensor_Sound block="Sound|pin %pin"
-    //% weight=99
-    //% blockGap=20
-    //% color="#228B22"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
-    export function Sound(pin: AnalogPin): number {
-        let value: number;
-        value = pins.analogReadPin(pin);
-        return value;
-    }
-
+    
     //% blockId=CrocoKit_Sensor_GetRGBValue block="GetRGBValue|value %value"
     //% blockGap=20
     //% weight=98
@@ -138,62 +115,5 @@ namespace CrocoKit_Sensor {
                 break;
         }
         return 0;
-    }
-
-    //% blockId=CrocoKit_Sensor_Ultrasonic block="Ultrasonic|Trig %Trig|Echo %Echo"
-    //% color="#228B22"
-    //% weight=97
-    //% blockGap=20
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
-    export function Ultrasonic(Trig: DigitalPin, Echo: DigitalPin): number {
-        let list: Array<number> = [0, 0, 0, 0, 0];
-        for (let i = 0; i < 5; i++) {
-            pins.setPull(Trig, PinPullMode.PullNone);
-            pins.digitalWritePin(Trig, 0);
-            control.waitMicros(2);
-            pins.digitalWritePin(Trig, 1);
-            control.waitMicros(15);
-            pins.digitalWritePin(Trig, 0);
-
-            let d = pins.pulseIn(Echo, PulseValue.High, 43200);
-            list[i] = Math.floor(d / 40);
-        }
-        list.sort();
-        let length = (list[1] + list[2] + list[3]) / 3;
-        return Math.floor(length);
-    }
-
-
-    //% blockId=CrocoKit_Sensor_IR block="IR|pin %pin|value %value"
-    //% weight=96
-    //% blockGap=20
-    //% color="#228B22"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
-    export function IR(pin: DigitalPin, value: enObstacle): boolean {
-        pins.setPull(pin, PinPullMode.PullUp);
-        return pins.digitalReadPin(pin) == value;
-    }
-
-    //% blockId=CrocoKit_Sensor_Vibration block="Vibration|pin %pin|get "
-    //% weight=95
-    //% blockGap=20
-    //% color="#228B22"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
-    export function Vibration(pin: DigitalPin, handle: () => void): void {
-        pins.setPull(pin, PinPullMode.PullUp);
-        pins.setEvents(pin, PinEventType.Pulse);
-        pins.onPulsed(pin, PulseValue.High, handle);
-    }
-
-    //% blockId=CrocoKit_Sensor_Hall block="Hall|pin %pin|get "
-    //% weight=94
-    //% blockGap=20
-    //% color="#228B22"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
-    export function Hall(pin: DigitalPin, handle: () => void): void {
-        pins.setPull(pin, PinPullMode.PullUp);
-        pins.setEvents(pin, PinEventType.Pulse);
-        pins.onPulsed(pin, PulseValue.High, handle);
-
     }
 }
