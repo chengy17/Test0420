6b10b3416d27456613303e00f54cf2bfc2d8caa2
chengy17 <chengy_95@163.com>
2019-07-23 09:05:20 +0800

    提交

--- auto.sh
+++ auto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git add .
-git commit --date=$(date "+%H%M%S") -am "autoCommit"
+#git commit --date=$(date "+%H%M%S") -am "autoCommit"
+git commit --date="Jul 23 9:05:20 2019 +0800" -am "提交"
 git push
 
