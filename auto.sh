20c4fff98c99d162f36c6f9a3bfce268699e852a
chengy17 <chengy_95@163.com>
2019-07-23 12:12:49 +0800

    [#id] title
    [问题描述]
    1.
    
    [问题原因]
    1.test
    
    [解决方案]
    1.

--- auto.sh
+++ auto.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-git add .
-git commit -m "commitInfo"
-git push
+#git add .
+#git commit -m "commitInfo"
+#git push
 
-#git config --global commit.templte
-#/g/microbit/projects/Test0420/templte
+#git config --global commit.templte ./templte
 #git commit
 
