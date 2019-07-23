8a9bc44e33ee1758c31f76a577967ad3aa6d32ff
chengy17 <chengy_95@163.com>
2019-07-23 11:35:09 +0800

    commitInfo

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-let curTime=$(date "+%H%M%S")
-let commitInfo = "autoCommit"+curTime
+curTime=$(date "+%H%M%S")
+commitInfo = "autoCommit:"+curTime
 git add .
 git commit -m commitInfo
 git push
